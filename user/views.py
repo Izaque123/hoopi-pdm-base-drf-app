c0df38070fb39ab0d754302e9c153bae6fbdea90
Izaque123 <isaquevieira2006@gmail.com>
2025-11-27 11:58:35 -0300

    feat: User CRUD

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer, UserCreateSerializer
+from drf_spectacular.utils import (
+    extend_schema,
+)
 # Create your views here.
+
+@extend_schema(tags=['User.Views'])
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer
+        return UserSerializer
